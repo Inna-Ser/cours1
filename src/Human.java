a0b0cf448716e1c50a4645cebeea57e9d327814d
Inna <serebryakovainna@mail.ru>
2022-12-15 03:59:41 +0200

    Add commit 2

--- src/Human.java
+++ src/Human.java
@@ -4,14 +4,25 @@ public class Human {
     public int yearOfBirth;
     public String jobTitle;
 
-    Human(String name, String city, int bornYeas, String jobTitle) {
-        this.name = name;
-        this.town = city;
-        this.yearOfBirth = bornYeas;
-        this.jobTitle = jobTitle;
+    Human(String name, String town, int yearOfBirth, String jobTitle) {
+        if (name != null) {
+            this.name = name;
+        } else
+            this.name = "Информация не указана";
+        if (town != null) {
+            this.town = town;
+        } else
+            this.town = "Информация не указана";
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (jobTitle != null) {
+            this.jobTitle = jobTitle;
+        } else
+            this.jobTitle = "Информация не указана";
     }
 
     void hello() {
-        System.out.println("Привет! Меня зовут " + name + ".\nЯ из города " + town + ".\nЯ родился в " + yearOfBirth + ". \nЯ работаю на должности " + jobTitle + ".\nБудем знакомы!\n");
+        System.out.println("Привет! Меня зовут " + this.name + ".\nЯ из города " + this.town + ".\nЯ родился в " + this.yearOfBirth + ". \nЯ работаю на должности " + this.jobTitle + ".\nБудем знакомы!\n");
     }
 }
